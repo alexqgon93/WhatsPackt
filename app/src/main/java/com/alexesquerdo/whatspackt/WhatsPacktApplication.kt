d886f5fceda6cd41959f938959d6cfddc5a22d1a
Alex Esquerdo <alexqgon93@gmail.com>
2025-08-05 11:45:13 -0300

    feat: setup base modular architecture with initial modules

---
+++ app/src/main/java/com/alexesquerdo/whatspackt/WhatsPacktApplication.kt
@@ -0,0 +1,12 @@
+package com.alexesquerdo.whatspackt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WhatsPacktApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
